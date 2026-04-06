ab55de4c71a7ed3b1101e42ae02f04ee001cd3cc
Boikuntha <boikuntha.dev@gmail.com>
2026-04-06 21:52:39 +0530

    Add ride eligibility checks based on height and credits
    Implement height and credits validation for ride eligibility.

---
+++ 03-Control Flow/the_cyclone.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+height = int(input('What is your height (cm)? '))
+credits = int(input('How many credits do you have? '))
+
+if height > 137 and credits > 10: 
+  print("Enjoy the ride!")
+elif height < 137 and credits > 10: 
+  print("You are not tall enough to ride.")
+elif height > 137 or credits < 10: 
+  print("You don't have enough credits.")
+else:
+  print("sorry, You have not met either requirement.") 
